b30c33b9adb8bc2ea862e1581d70d8eba3a247cc
MinDongIl <ehddlf9685@gmail.com>
2026-03-05 01:41:33 +0900

    infra: Add CPU usage alert policy (>70%) to monitoring.tf

--- monitoring.tf
+++ monitoring.tf
@@ -3,7 +3,7 @@ resource "google_monitoring_notification_channel" "email_alert" {
   display_name = "Mervis SRE Email Alert"
   type         = "email"
   labels = {
-    email_address = "ehddlf9685@gmail.com" #
+    email_address = "ehddlf9685@gmail.com"
   }
 }
 
@@ -15,9 +15,8 @@ resource "google_monitoring_alert_policy" "disk_space_high" {
   conditions {
     display_name = "Disk usage is over 80%"
     condition_threshold {
-      # Ops Agent가 수집하는 디스크 사용량(%) 지표
       filter          = "metric.type=\"agent.googleapis.com/disk/percent_used\" AND resource.type=\"gce_instance\" AND metric.labels.state=\"used\""
-      duration        = "60s" # 테스트를 위해 1분간 지속 시 즉시 알림 발송
+      duration        = "60s"
       comparison      = "COMPARISON_GT"
       threshold_value = 80.0
       
@@ -28,5 +27,28 @@ resource "google_monitoring_alert_policy" "disk_space_high" {
     }
   }
   
+  notification_channels = [google_monitoring_notification_channel.email_alert.id]
+}
+
+# 3. CPU 사용률 70% 초과 경보 정책
+resource "google_monitoring_alert_policy" "cpu_usage_high" {
+  display_name = "Mervis CPU Usage Alert (> 70%)"
+  combiner     = "OR"
+
+  conditions {
+    display_name = "CPU utilization is over 70%"
+    condition_threshold {
+      filter          = "metric.type=\"compute.googleapis.com/instance/cpu/utilization\" AND resource.type=\"gce_instance\""
+      duration        = "60s"
+      comparison      = "COMPARISON_GT"
+      threshold_value = 0.7
+      
+      aggregations {
+        alignment_period   = "60s"
+        per_series_aligner = "ALIGN_MEAN"
+      }
+    }
+  }
+
   notification_channels = [google_monitoring_notification_channel.email_alert.id]
 }
